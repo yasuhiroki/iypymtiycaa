4dd2b54123791514527ed81c1dfe7b539478cfa8
yasuhiroki <yasuhiroki.duck@gmail.com>
2015-04-12 23:14:35 +0900

    Add rbenv install script

---
+++ install-rbenv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+sudo apt install -y git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+
+echo "Install rbenv"
+if [ -d ~/.rbenv ]; then
+    (
+    cd ~/.rbenv
+    git pull
+    )
+else
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+fi
+
+rbenvrc=".rbenvrc"
+grep -sq rbenv ~/${rbenvrc} || echo 'export PATH="$HOME/.rbenv/bin:$PATH"' > ~/${rbenvrc}
+grep -sq rbenv ~/${rbenvrc} || echo 'eval "$(rbenv init -)"' >> ~/${rbenvrc}
+grep -sq rbenv ~/${rbenvrc} || echo 'alias be="bundle exec"' >> ~/${rbenvrc}
+
+grep -sq "${rbenvrc}" ~/.bashrc || echo "source ~/${rbenvrc}" >> ~/.bashrc
+
+if [ -d ~/.rbenv/plugins/ruby-build ]; then
+    (
+    cd ~/.rbenv/plugins/ruby-build
+    git pull
+    )
+else
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
+
+
