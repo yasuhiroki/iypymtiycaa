041c81d246c36bdd7fda3cff42b0b701ed457a10
yasuhiroki <yasuhiroki.duck@gmail.com>
2015-05-21 23:28:12 +0900

    Install jenkins script for debian and redhat os

---
+++ install-jenkins.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+[ `whoami` != "root" ] && echo "Should be run with sudo" && exit 1
+
+lsb_dist=''
+if `command lsb_release >>/dev/null`; then
+    lsb_dist="$(lsb_release -si)"
+fi
+if [ -z "$lsb_dist" ] && [ -r /etc/lsb-release ]; then
+    lsb_dist="$(. /etc/lsb-release && echo "$DISTRIB_ID")"
+fi
+if [ -z "$lsb_dist" ] && [ -r /etc/debian_version ]; then
+    lsb_dist='debian'
+fi
+if [ -z "$lsb_dist" ] && [ -r /etc/fedora-release ]; then
+    lsb_dist='fedora'
+fi
+if [ -z "$lsb_dist" ] && [ -r /etc/os-release ]; then
+    lsb_dist="$(. /etc/os-release && echo "$ID")"
+fi
+
+lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+case "$lsb_dist" in
+    amzn|fedora|centos)
+        wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+        rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+        yum install jenkins
+        exit 0
+        ;;
+
+    ubuntu|debian|linuxmint|solydxk)
+        wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+        sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+        apt-get update
+        apt-get install jenkins
+        exit 0
+        ;;
+    *)
+        echo "Not supported yet"
+        exit 1
+        ;;
+esac
