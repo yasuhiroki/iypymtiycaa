f4cbd6c04cb170848a51cb3c971f263eb45d2635
yasuhiroki <yasuhiroki.duck@gmail.com>
2015-11-08 12:30:46 +0900

    Add enhancd command install

---
+++ install/install-enhancd.sh
@@ -0,0 +1,27 @@
+#!/bin/bsah
+
+install_dir=".enhancd"
+
+if [ -d ~/${install_dir} ]; then
+    (
+    cd ~/${install_dir}
+    git pull
+    )
+else
+    git clone https://github.com/b4b4r07/enhancd.git ~/${install_dir}
+fi
+
+if [ "$(uname)" == 'Darwin' ]; then
+    brew install peco
+else
+    echo "go get github.com/peco/peco/cmd/peco"
+fi
+
+rc=".enhancdrc"
+echo "ENHANCD_COMMAND='c'
+source ~/${install_dir}/enhancd.sh
+" > ~/${rc}
+[ -f ~/.bashrc ] && grep -sq "^source.*${rc}$" ~/.bashrc || echo "source ~/${rc}" >> ~/.bashrc
+[ -f ~/.zshrc ]  && grep -sq "^source.*${rc}$" ~/.zshrc || echo "source ~/${rc}" >> ~/.zshrc
+
+
