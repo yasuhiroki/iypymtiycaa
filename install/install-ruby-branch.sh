099564fa7a1125437347e63ccd55700fda319fd5
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-01-26 21:21:41 +0900

    Added install ruby from repository

---
+++ install/install-ruby-branch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+version="${1:-trunk}"
+
+source_dir=".ruby"
+[ ! -d ${source_dir} ] && git clone https://github.com/ruby/ruby.git ~/${source_dir}
+
+(
+  cd ${source_dir}
+  git clean -xdf
+  git checkout ${version}
+  git pull
+  autoconf
+  ./configure --prefix=$HOME/.rbenv/versions/${version} --enable-shared --enable-debug-env CPPFLAGS='-DRUBY_DEBUG_ENV -DARRAY_DEBUG'
+  make
+  make check
+  make install
+)
+
