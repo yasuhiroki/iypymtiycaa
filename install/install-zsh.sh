9f579c6363f611ea2a9f7cdd212357cefbe765cd
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-05-18 21:40:21 +0900

    Added zsh-autosuggestions

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+script_dir=$(dirname $0)
+
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-cp zsh/my-zshrc.zsh ~/.oh-my-zsh/custom/
+cp ${script_dir}/zsh/my-zshrc.zsh ~/.oh-my-zsh/custom/
+
+
+#zsh-autosuggestions
+if [ $ZSH_CUSTOM ] && [ ! -d $ZSH_CUSTOM/plugins/zsh-autosuggestions ]; then
+  git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
 
