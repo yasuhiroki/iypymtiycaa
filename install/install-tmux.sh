5f998372cadf13fde23bdfd7380840c624fedba4
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-05-28 13:31:19 +0900

    Updated tmux conf.

--- install/install-tmux.sh
+++ install/install-tmux.sh
@@ -9,3 +9,12 @@ fi
 cp $(dirname $0)/tmux/tmux.conf ~/.tmux.conf
 touch ~/.tmux.conf.local
 
+local_conf=".tmux.conf.local"
+if is_osx ;then
+  os_conf="tmux.conf.mac"
+else
+  os_conf="tmux.conf.ubuntu"
+fi
+cp $(dirname $0)/tmux/${os_conf} ~/.${os_conf}
+grep -sq ".${os_conf}" ~/${local_conf} && echo "source-file ~/.${os_conf}" >> ~/${local_conf}
+
