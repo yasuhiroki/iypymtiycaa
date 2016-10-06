961c2d2b54016e51bcef6feb0214f4139f57f2e2
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-10-07 01:41:05 +0900

    Support debian for ag command.

--- install/_func.sh
+++ install/_func.sh
@@ -1,3 +1,7 @@
 function is_osx() {
     [[ $(uname) =~ "Darwin" ]]
 }
+
+function is_debian() {
+    [[ -e "/etc/debian_version" -o -e "/etc/lsb-release" ]]
+}
--- install/install-ag.sh
+++ install/install-ag.sh
@@ -1,4 +1,13 @@
-brew install ag
+. $(dirname $0)/_func.sh
+
+if is_osx ;then
+  brew install ag
+elif is_debian; then
+  apt install silversearcher-ag
+else
+  exit 0
+fi
+
 echo "*~
 *.swp
 
