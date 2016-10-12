73fac2bd56f0fcd9f327e938eab5a0a5ab5ac9b6
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-10-13 00:55:01 +0900

    Cosmetic

--- install/install-pry-tools.sh
+++ install/install-pry-tools.sh
@@ -1,7 +1,7 @@
 gems="pry pry-nav"
 if ruby -v | awk '{print }' | grep -sq 1.9 ; then
-    gems="$gems pry-debugger"
+  gems="$gems pry-debugger"
 else
-    gems="$gems pry-byebug"
+  gems="$gems pry-byebug"
 fi
 gem install $gems
