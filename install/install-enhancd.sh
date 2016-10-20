e5670737f6f9134921f27f8cd69ad5dcad529f5e
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-10-21 07:50:13 +0900

    cosmetic.

--- install/install-enhancd.sh
+++ install/install-enhancd.sh
@@ -3,12 +3,12 @@
 install_dir=".enhancd"
 
 if [ -d ~/${install_dir} ]; then
-    (
-    cd ~/${install_dir}
-    git pull
-    )
+  (
+  cd ~/${install_dir}
+  git pull
+  )
 else
-    git clone https://github.com/b4b4r07/enhancd.git ~/${install_dir}
+  git clone https://github.com/b4b4r07/enhancd.git ~/${install_dir}
 fi
 
 echo "Install fzf"
