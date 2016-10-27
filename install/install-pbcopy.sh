dadb473c334df0354bac1809db9feb1e1bb4de46
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-10-27 19:24:44 +0900

    Update install-pbcopy.

--- install/install-pbcopy.sh
+++ install/install-pbcopy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ "$(uname)" != 'Darwin' ]; then
-    apt install xsel
-    grep "^alias pobcopy.*" ~/.bash_aliases || echo "alias pbcopy='xsel --clipboard --input'" >> ~/.bash_aliases
+. $(dirname $0)/_func.sh
+
+if is_debian ;then
+  apt install xsel
+  grep "^alias pobcopy.*" ~/.bash_aliases || echo "alias pbcopy='xsel --clipboard --input'" >> ~/.bash_aliases
 fi
