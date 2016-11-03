6f5635dacbe4cc228e3b38bad9cbeb81707705e8
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-11-03 21:41:57 +0900

    Fix mistake.

--- install/install-go.sh
+++ install/install-go.sh
@@ -1,6 +1,6 @@
 . $(dirname $0)/_func.sh
 
-if is_osx; then
+if is_debian; then
   sudo apt-get install curl git mercurial make binutils bison gcc build-essential
   bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 fi
