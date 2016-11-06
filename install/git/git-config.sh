a1579e9da9cd8e4c4150db5a8c41a9a26a7104db
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-11-06 11:40:32 +0900

    Add ~/.gitignore_global

--- install/git/git-config.sh
+++ install/git/git-config.sh
@@ -1,4 +1,7 @@
 git config --global push.default simple
 git config --global core.autocrlf input
+echo "*~
+" > ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
 git config --global rebase.autostash true
 
