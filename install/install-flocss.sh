29ada07f284c709a7a3024e59efd3ee563bbc717
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-11-07 22:10:09 +0900

    Fix install-flocss.sh, don't clone gist repository

--- install/install-flocss.sh
+++ install/install-flocss.sh
@@ -1,6 +1,5 @@
 mkdir -p ~/mybin/
 if [ ! -f ~/mybin/mkflocss ]; then
-  git clone https://gist.github.com/1992d40007e40d50b44501546dd1a4ee.git ~/mybin/mkflocss-repo
-  cp ~/mybin/{mkflocss-repo/mkflocss.sh,mkflocss}
+  curl https://gist.githubusercontent.com/yasuhiroki/1992d40007e40d50b44501546dd1a4ee/raw/dda00dc392d02b1afe6d35c10612fdb13fef7523/mkflocss.sh -o ~/mybin/mkflocss
   chmod 755 ~/mybin/mkflocss
 fi
