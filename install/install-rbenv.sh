1dff1375ec62383378998503a0d15446e05bf42d
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-12-04 09:15:27 +0900

    Fix error install-rbenv.sh

--- install/install-rbenv.sh
+++ install/install-rbenv.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash
+
+. $(dirname $0)/_func.sh
 
 if is_debian; then
   sudo apt install -y git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
