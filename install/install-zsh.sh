b7afcc16378de2c35766bd6e08eae620eb48aa79
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-01-31 23:59:25 +0900

    cosmetic

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -18,11 +18,12 @@ backup_lotation ~/.zshrc
 if is_osx; then
   echo 'export ZPLUG_HOME=/usr/local/opt/zplug'  > ~/.zshrc
   echo 'source $ZPLUG_HOME/init.zsh'            >> ~/.zshrc
+  echo ''                                       >> ~/.zshrc
 elif is_debian; then
   echo 'source ~/.zplug/init.zsh'  > ~/.zshrc
   echo ''                         >> ~/.zshrc
 fi
-cat ${script_dir}/zsh/zshrc     >> ~/.zshrc
+cat ${script_dir}/zsh/zshrc >> ~/.zshrc
 cp ${script_dir}/zsh/my-zshrc.zsh ~/.my-zshrc.zsh
 
 update_rc $(dirname $0)/rc/alias.sh
