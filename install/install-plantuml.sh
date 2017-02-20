22e42f1c2803e67e148ec04d05d533f5b9402742
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-02-21 01:04:19 +0900

    Fix install-plantuml.sh, no use wget

--- install/install-plantuml.sh
+++ install/install-plantuml.sh
@@ -8,7 +8,7 @@ type java >/dev/null || {
 mkdir -p ~/mybin
 
 [ -e ~/mybin/plantuml.jar ] || {
-  wget 'http://downloads.sourceforge.net/project/plantuml/plantuml.jar?r=http%3A%2F%2Fja.plantuml.com%2Fdownload.html&ts=1459694959&use_mirror=jaist' -O ~/mybin/plantuml.jar
+  curl 'http://downloads.sourceforge.net/project/plantuml/plantuml.jar?r=http%3A%2F%2Fja.plantuml.com%2Fdownload.html&ts=1459694959&use_mirror=jaist' -o ~/mybin/plantuml.jar
 }
 
 cat <<EOH > ~/mybin/plantuml
