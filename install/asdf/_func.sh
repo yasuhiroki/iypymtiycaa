1374f6786c8e64efc5f57f542770117cf7123686
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-03-30 01:04:08 +0900

    Add install node in asdf

--- install/asdf/_func.sh
+++ install/asdf/_func.sh
@@ -11,6 +11,9 @@ function default_install() {
   local plugin_url="$3"
 
   asdf plugin-add ${lang} ${plugin_url}
+  if [ -f ~/.asdf/plugins/${lang}/bin/import-release-team-keyring ]; then
+    bash ~/.asdf/plugins/${lang}/bin/import-release-team-keyring
+  fi
 
   asdf install ${lang} ${version}
   asdf global  ${lang} ${version}
