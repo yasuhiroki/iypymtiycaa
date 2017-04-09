1da03f4a8d97e9cfe13fd335c5721d16eeaf3a39
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-04-10 00:49:26 +0900

    Fix ruby trunk build option

--- install/install-ruby-branch.sh
+++ install/install-ruby-branch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+. $(dirname $0)/_func.sh
+
 version="${1:-trunk}"
 
 source_dir=".ruby"
 [ ! -d ~/${source_dir} ] && git clone https://github.com/ruby/ruby.git ~/${source_dir}
+
+if is_osx; then
+  BUILD_OPT="--enable-shared --with-readline-dir=$(brew --prefix readline) --with-openssl-dir=$(brew --prefix openssl)"
+fi
 
 (
   cd ~/${source_dir}
@@ -11,7 +17,7 @@ source_dir=".ruby"
   git checkout ${version}
   git pull
   autoconf
-  ./configure --prefix=$HOME/.rbenv/versions/${version} --enable-shared --enable-debug-env CPPFLAGS='-DRUBY_DEBUG_ENV -DARRAY_DEBUG'
+  ./configure --prefix=$HOME/.rbenv/versions/${version} ${BUILD_OPT} --enable-debug-env CPPFLAGS='-DRUBY_DEBUG_ENV -DARRAY_DEBUG'
   make
   make check
   make install
