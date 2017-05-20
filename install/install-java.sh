8bae9c81901437f12ea2375166e7ef11a317831d
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-05-20 17:24:04 +0900

    Remove java/pip install script.

--- install/install-java.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. $(dirname $0)/_func.sh
-
-type java >/dev/null || {
-  if is_debian; then
-    apt -y install openjdk-8-jre
-  fi
-}
--- install/install-pip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. $(dirname $0)/_func.sh
-
-is_installed "pip" || {
-  if is_debian; then
-    apt -y install python-pip
-  fi
-}
