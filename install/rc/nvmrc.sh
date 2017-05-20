3917bfd601621de62266fddcbd959a4968f2fd4e
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-05-20 16:51:23 +0900

    Modify, node managed by zshef

--- install/rc/nvmrc.sh
+++
@@ -1,4 +0,0 @@
-[ -d "$HOME/.nvm" ] && {
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-}
---
+++ zshef/cookbooks/asdf/_func.sh
@@ -0,0 +1,14 @@
+function my::asdf::default_install() {
+  local lang="$1"
+  local version="$2"
+  local plugin_url="$3"
+
+  asdf plugin-add ${lang} ${plugin_url}
+  if [ -f ~/.asdf/plugins/${lang}/bin/import-release-team-keyring ]; then
+    bash ~/.asdf/plugins/${lang}/bin/import-release-team-keyring
+  fi
+
+  asdf install ${lang} ${version}
+  asdf global  ${lang} ${version}
+}
+
