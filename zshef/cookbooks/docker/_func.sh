89a9f00cf423b954fd0b44d09760093590de3875
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-07-13 16:05:48 +0900

    Change way to run ruby command

--- zshef/cookbooks/docker/_func.sh
+++ zshef/cookbooks/docker/_func.sh
@@ -3,15 +3,19 @@
 function my::docker::language::init() {
   local docker_img="$1"
   local docker_run="$2"
-  local bin_dir="$3"
+  local docker_bin_dir="$3"
+  local local_bin_dir="$4"
 
-  local tmpfile=$(mktemp)
-  local bin_list=($(${=docker_run} ${=docker_img} ls --color=never -1 ${bin_dir} | tr -d '\r' | xargs))
+  : ${docker_img:?}
+  : ${docker_run:?}
+  : ${docker_bin_dir:?}
+  test -d ${local_bin_dir:?}
+
+  local bin_list=($(${=docker_run} ${=docker_img} ls --color=never -1 ${docker_bin_dir} | tr -d '\r' | xargs))
   for b in ${bin_list[@]}
   do
-    echo "alias $b='${=docker_run} -v \$(pwd):\$(pwd) -w \$(pwd) ${=docker_img} $b'" >> ${tmpfile}
+    echo "${docker_run} -v \$(pwd):\$(pwd) -w \$(pwd) ${docker_img} ${b} \${@}" > ${local_bin_dir}/${b}
+    chmod 755 ${local_bin_dir}/${b}
   done
-  cat ${tmpfile}
-  rm -f ${tmpfile}
 }
 
