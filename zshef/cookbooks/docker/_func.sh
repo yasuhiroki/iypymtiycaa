505083d688185efe73aaeef8e6538f592b8ec94c
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-07-16 18:49:02 +0900

    Enable to sepecify interactive mode

--- zshef/cookbooks/docker/_func.sh
+++ zshef/cookbooks/docker/_func.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env zsh
 
 function my::docker::language::init() {
-  local docker_img="$1"
-  local docker_run="$2"
-  local docker_bin_dir="$3"
+  local img="$1"
+  local run_opt="$2"
+  local bin_dir="$3"
   local local_bin_dir="$4"
 
-  : ${docker_img:?}
-  : ${docker_run:?}
-  : ${docker_bin_dir:?}
+  local interactive_only_bin_list="$5"
+
+  : ${img:?}
+  : ${run_opt:?}
+  : ${bin_dir:?}
   test -d ${local_bin_dir:?}
 
-  local bin_list=($(${=docker_run} ${=docker_img} ls --color=never -1 ${docker_bin_dir} | tr -d '\r' | xargs))
+  local bin_list=($(docker run ${=run_opt} ${=img} ls --color=never -1 ${bin_dir} | tr -d '\r' | xargs))
   for b in ${bin_list[@]}
   do
-    echo "${docker_run} -v \$(pwd):\$(pwd) -w \$(pwd) ${docker_img} ${b} \${@}" > ${local_bin_dir}/${b}
+    echo ${interactive_only_bin_list} | grep -sq ${b} && {
+      echo "docker run ${run_opt} -t -v \$(pwd):\$(pwd) -w \$(pwd) ${img} ${b} \${@}" > ${local_bin_dir}/${b}
+    } || {
+      echo "docker run ${run_opt} -v \$(pwd):\$(pwd) -w \$(pwd) ${img} ${b} \${@}" > ${local_bin_dir}/${b}
+    }
     chmod 755 ${local_bin_dir}/${b}
   done
 }
