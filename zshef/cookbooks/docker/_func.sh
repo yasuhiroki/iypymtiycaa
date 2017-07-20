06db2bac3efdc19b486ff4e350a80ec0100d625d
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-07-20 14:32:19 +0900

    Fix bin file generator

--- zshef/cookbooks/docker/_func.sh
+++ zshef/cookbooks/docker/_func.sh
@@ -6,8 +6,6 @@ function my::docker::language::init() {
   local bin_dir="$3"
   local local_bin_dir="$4"
 
-  local interactive_only_bin_list="$5"
-
   : ${img:?}
   : ${run_opt:?}
   : ${bin_dir:?}
@@ -16,12 +14,28 @@ function my::docker::language::init() {
   local bin_list=($(docker run ${=run_opt} ${=img} ls --color=never -1 ${bin_dir} | tr -d '\r' | xargs))
   for b in ${bin_list[@]}
   do
-    echo ${interactive_only_bin_list} | grep -sq ${b} && {
-      echo "docker run ${run_opt} -t -v \$(pwd):\$(pwd) -w \$(pwd) ${img} ${b} \${@}" > ${local_bin_dir}/${b}
-    } || {
-      echo "docker run ${run_opt} -v \$(pwd):\$(pwd) -w \$(pwd) ${img} ${b} \${@}" > ${local_bin_dir}/${b}
-    }
+    my::docker::bin::body \
+        "${img}" \
+        "${run_opt} -v \$(pwd):\$(pwd) -w \$(pwd)" \
+        "${b} \"\${@}\"" > ${local_bin_dir}/${b}
     chmod 755 ${local_bin_dir}/${b}
   done
+}
+
+function my::docker::bin::body() {
+  local img="$1"
+  local run_opt="$2"
+  local cmd="$3"
+  : ${img:?}
+  : ${run_opt:?}
+  : ${cmd:?}
+cat <<EOH
+if [ -t 0 ]; then
+  opt="-t"
+else
+  opt=""
+fi
+docker run ${run_opt} \${opt} ${img} ${cmd}
+EOH
 }
 
