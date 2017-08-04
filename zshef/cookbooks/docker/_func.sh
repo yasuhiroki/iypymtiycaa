df35961a5f1bc6558b4ee5418da704b556f09a21
yasuhiroki <yasuhiroki.duck@gmail.com>
2017-08-04 23:53:25 +0900

    Fix indent

--- zshef/cookbooks/docker/_func.sh
+++ zshef/cookbooks/docker/_func.sh
@@ -6,64 +6,64 @@ function my::docker::build() {
     local image_name="$2"
     local dockerfile="${3:-Dockerfile}"
     (
-        cd ${base_dir}
-        docker build --file ${dockerfile} -t ${image_name} .
+    cd ${base_dir}
+    docker build --file ${dockerfile} -t ${image_name} .
     )
 }
 
 function my::docker::language::init() {
-  local img="$1"
-  local run_opt="$2"
-  local bin_dir="$3"
-  local local_bin_dir="$4"
+    local img="$1"
+    local run_opt="$2"
+    local bin_dir="$3"
+    local local_bin_dir="$4"
 
-  : ${img:?}
-  : ${run_opt:?}
-  : ${bin_dir:?}
-  test -d ${local_bin_dir:?}
+    : ${img:?}
+    : ${run_opt:?}
+    : ${bin_dir:?}
+    test -d ${local_bin_dir:?}
 
-  local bin_list=($(docker run ${=run_opt} ${=img} ls --color=never -1 ${bin_dir} | tr -d '\r' | xargs))
-  for b in ${bin_list[@]}
-  do
-    my::docker::bin::create \
-        "${img}" \
-        "$(_my::docker::bin::option "${run_opt}")" \
-        "${local_bin_dir}" \
-        "${b}" \
-        "${b} \"\${@}\""
-  done
+    local bin_list=($(docker run ${=run_opt} ${=img} ls --color=never -1 ${bin_dir} | tr -d '\r' | xargs))
+    for b in ${bin_list[@]}
+    do
+        my::docker::bin::create \
+            "${img}" \
+            "$(_my::docker::bin::option "${run_opt}")" \
+            "${local_bin_dir}" \
+            "${b}" \
+            "${b} \"\${@}\""
+    done
 }
 
 function my::docker::bin::create() {
-  local img="$1"
-  local run_opt="$2"
-  local local_bin_dir="$3"
-  local bin_file="$4"
-  : ${bin_file:?}
-  local cmd="$5"
-  : ${cmd:?}
+    local img="$1"
+    local run_opt="$2"
+    local local_bin_dir="$3"
+    local bin_file="$4"
+    : ${bin_file:?}
+    local cmd="$5"
+    : ${cmd:?}
 
-  mkdir -p ${local_bin_dir:?} >& /dev/null
+    mkdir -p ${local_bin_dir:?} >& /dev/null
 
-  _my::docker::bin::body \
-      "${img}" \
-      "${run_opt}" \
-      "${cmd}"     > ${local_bin_dir}/${bin_file}
-  chmod 755          ${local_bin_dir}/${bin_file}
+    _my::docker::bin::body \
+        "${img}" \
+        "${run_opt}" \
+        "${cmd}"     > ${local_bin_dir}/${bin_file}
+    chmod 755          ${local_bin_dir}/${bin_file}
 }
 
 function _my::docker::bin::option() {
-  echo "${1} -v \$(pwd -P):\$(pwd -P) -w \$(pwd -P)"
+    echo "${1} -v \$(pwd -P):\$(pwd -P) -w \$(pwd -P)"
 }
 
 function _my::docker::bin::body() {
-  local img="$1"
-  local run_opt="$2"
-  local cmd="$3"
-  : ${img:?}
-  : ${run_opt:?}
-  : ${cmd:?}
-cat <<EOH
+    local img="$1"
+    local run_opt="$2"
+    local cmd="$3"
+    : ${img:?}
+    : ${run_opt:?}
+    : ${cmd:?}
+    cat <<EOH
 if [ -t 0 ]; then
   opt="-t"
 else
