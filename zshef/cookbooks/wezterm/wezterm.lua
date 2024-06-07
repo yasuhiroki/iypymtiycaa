c025e59f73c91299a2309d8f67d8ae981eea25cd
yasuhiroki <yasuhiroki.duck@gmail.com>
2024-06-07 23:06:16 +0900

    Use HackGen35 in wezterm

--- zshef/cookbooks/wezterm/wezterm.lua
+++ zshef/cookbooks/wezterm/wezterm.lua
@@ -27,7 +27,7 @@ config.window_padding = {
 }
 config.color_scheme = 'Solarized Dark - Patched'
 config.leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1000 }
-config.font = wezterm.font("Monaco")
+config.font = wezterm.font("HackGen35")
 config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
 config.keys = {
     { key = "a", mods = "CMD|SHIFT", action = act.ActivateCommandPalette },
