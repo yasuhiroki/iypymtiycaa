2781c16b8abf202bc9a81622d8a637e4812737b9
yasuhiroki <yasuhiroki.duck@gmail.com>
2025-05-13 22:07:03 +0900

    fix asdf sub command breaking change

--- zshef/cookbooks/asdf/_func.sh
+++ zshef/cookbooks/asdf/_func.sh
@@ -13,13 +13,13 @@ function my::asdf::default_install() {
   local version="$2"
   local plugin_url="$3"
 
-  asdf plugin-add ${lang} ${plugin_url}
+  asdf plugin add ${lang} ${plugin_url}
   if [ -f ~/.asdf/plugins/${lang}/bin/import-release-team-keyring ]; then
     bash ~/.asdf/plugins/${lang}/bin/import-release-team-keyring
   fi
 
   asdf install ${lang} ${version}
-  asdf global  ${lang} ${version}
+  asdf set -u  ${lang} ${version}
 }
 
 function my::asdf::rc::name() {
